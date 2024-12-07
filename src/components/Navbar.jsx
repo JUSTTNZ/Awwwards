685fcf30117763067ebf2665de012b7a8d08b598
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-12-07 09:49:14 -0800

    almost done with the bentotilt and bentocards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,42 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useWindowScroll } from 'react-use';
 import { TiLocationArrow } from 'react-icons/ti';
 import Button from './Button';
+import gsap from 'gsap';
 const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact']
 
 
 const Navbar = () => {
     const [isAudioPlaying, setIsAudioPlaying] =useState(false);
     const [isIndicatorActive, setIsIndicatorActive] = useState(false)
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [isNavVisible, setIsNavVisible] = useState(true)
     const navContainerRef = useRef(null);
     const audioElementRef = useRef(null);
 
-    const { y}
+    const { y: currentScrollY } = useWindowScroll();
+
+    useEffect(() => {
+        if(currentScrollY === 0) {
+            setIsNavVisible(true)
+            navContainerRef.current.classList.remove('floating-nav');
+        }else if(currentScrollY > lastScrollY) {
+            setIsNavVisible(false)
+            navContainerRef.current.classList.add('floating-nav')
+        } else if (currentScrollY < lastScrollY) {
+            setIsNavVisible(true) 
+            navContainerRef.current.classList.add('floating-nav')
+        }
+
+        setLastScrollY(currentScrollY)
+    },[currentScrollY, lastScrollY])
+
+    useEffect(() => {
+        gsap.to(navContainerRef.current , {
+            y: isNavVisible ? 0 : -100,
+            opacity: isNavVisible ? 1 : 0,
+            duration: 0.2,
+        })
+    })
     const toggleAudioIndicator = () =>  {
         setIsAudioPlaying((prev) => !prev);
 
@@ -35,7 +61,7 @@ const Navbar = () => {
                         id='product-button'
                         title='Products'
                         rightIcon={<TiLocationArrow/>}
-                        containerClass= "bg-blue-50 md:flex items-center justify-center gap-1"
+                        containerClass= "bg-blue-50 md:flex items-center justify-center gap-1  hidden"
                     />
                 </div>
                 <div className='flex h-full items-center'>
